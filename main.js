a417288e2a223643027f5f586427f23af824b606
JaeHongMin94 <moonshin123@naver.com>
2021-06-21 18:50:17 +0900

    Add RegExp method

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+const str = `
+  010-1234-5678
+  thesecon@gmail.com
+  https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+  The quick brown fox jumps over the lazy dog.
+  abbcccdddd
+`;
+
+// const regexp = new RegExp('the', 'g');
+const regexp = /fox/gi;
+console.log(regexp.test(str));
+console.log(str.match(regexp));
+console.log(str.replace(regexp, 'dog'));
